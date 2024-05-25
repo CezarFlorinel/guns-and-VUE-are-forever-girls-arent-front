c2f76503d51163f50fe7afc9df1ab8c6b261b371
CezarFlorinel <706431@student.inholland.nl>
2024-05-25 17:12:06 +0200

    she almost finished all the juice

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <navigation />
+  <router-view />
+</template>
+
+<script>
+import Navigation from './components/commons/Navigation.vue';
+import { useLoggedInStore } from '@/stores/logged_in';
+
+export default {
+  name: "App",
+  components: {
+    Navigation
+  },
+  created() {
+    const store = useLoggedInStore();
+    store.autoLogin();
+  }
+};
+</script>
+
+<style>
+/* Style as needed */
+</style>
+
