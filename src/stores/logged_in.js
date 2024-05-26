cbd28dc1398038b3b2018c46af59eef44217d6d7
CezarFlorinel <706431@student.inholland.nl>
2024-05-26 22:12:26 +0200

    sdas

--- src/stores/logged_in.js
+++ src/stores/logged_in.js
@@ -2,14 +2,19 @@ import { defineStore } from "pinia";
 import axios from "../axios_auth";
 
 export const useLoggedInStore = defineStore("logged_in", {
-
     state: () => ({
         username: localStorage.getItem('username') || '',
         token: localStorage.getItem('token') || '',
+        email: localStorage.getItem('email') || '',
+        admin: localStorage.getItem('admin') === 'true', // Convert to boolean
+        avatarId: localStorage.getItem('avatarId') || '',
     }),
     getters: {
         isLoggedIn: (state) => !!state.token && !!state.username,
         getToken: (state) => state.token,
+        getEmail: (state) => state.email,
+        isAdmin: (state) => state.admin,
+        getAvatarId: (state) => state.avatarId,
     },
     actions: {
         login(username, password) {
@@ -19,12 +24,20 @@ export const useLoggedInStore = defineStore("logged_in", {
                     password: password,
                 })
                     .then((response) => {
+                        console.log(response.data);
                         this.username = response.data.username;
                         this.token = response.data.token;
+                        this.email = response.data.email;
+                        this.admin = response.data.admin;
+                        this.avatarId = response.data.avatarId;
+
                         localStorage.setItem('token', response.data.token);
                         localStorage.setItem('username', response.data.username);
+                        localStorage.setItem('email', response.data.email);
+                        localStorage.setItem('admin', response.data.admin);
+                        localStorage.setItem('avatarId', response.data.avatarId);
+
                         axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
-                        console.log(response.data.token);
                         resolve();
                     })
                     .catch((error) => {
@@ -32,22 +45,25 @@ export const useLoggedInStore = defineStore("logged_in", {
                         reject();
                     });
             });
-
-
         },
         autoLogin() {
             const token = localStorage.getItem('token');
             const username = localStorage.getItem('username');
+            const email = localStorage.getItem('email');
+            const admin = localStorage.getItem('admin') === 'true';
+            const avatarId = localStorage.getItem('avatarId');
 
-            if (token && username) {
+            if (token && username && email && avatarId) {
                 axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
                 this.token = token;
                 this.username = username;
-            }
-            else {
-                console.log('no token or username');
+                this.email = email;
+                this.admin = admin;
+                this.avatarId = avatarId;
+            } else {
+                console.log('Missing token, username, email, or avatarId');
             }
         },
-
     },
 });
+
